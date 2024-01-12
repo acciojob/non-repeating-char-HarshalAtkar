71e14ffd3731cc1a8a58c9f556bd7746227f9bf4
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-01-12 13:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return s[i];
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
